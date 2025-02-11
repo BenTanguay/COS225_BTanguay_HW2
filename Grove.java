483661b9e2961418af439ab2c992cc4521c383f2
bpt06 <bentanguay06@gmail.com>
2025-02-11 11:32:11 -0500

    Create Grove.java
    Created Grove.java and added some of the initial code.

---
+++ Grove.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Grove {
+    public ArrayList<Tree> grove = new ArrayList<Tree>(16);
+    public String groveName;
+
+    public Grove(String groveName){
+        this.groveName = groveName;
+    }
+}
