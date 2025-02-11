1bc9e1253383544fd8c3eca81f9e70a48ad55bca
bpt06 <bentanguay06@gmail.com>
2025-02-11 11:28:21 -0500

    Create Tree.java
    Created the Tree.java file and added the required code.

---
+++ Tree.java
@@ -0,0 +1,11 @@
+public class Tree{
+    public int idNumber;
+    public int age;
+    public String speciesName;
+
+    public Tree(int idNumber, int age, String speciesName){
+        this.idNumber = idNumber;
+        this.age = age;
+        this.speciesName = speciesName;
+    }
+}
